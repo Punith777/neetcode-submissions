c59b964536d0d7e1d9b547de789af5c864cd1a8d
Punith777 <90909440+Punith777@users.noreply.github.com>
2026-06-24 10:58:58 +0530

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+
+        for(String str: strs){
+            char chars[] = str.toCharArray();
+            Arrays.sort(chars); // sort that particular string after converting to char
+
+            //again convert it back to the string
+            String key = new String(chars);
+
+            // if that particular sorted str is not already in the map, put that as key 
+            // and empty array as the value 
+            map.putIfAbsent(key, new ArrayList<>());
+            
+            //else if that key already exists, put that string for that key
+            map.get(key).add(str);
+
+        }
+        return new ArrayList<>(map.values());
+    }
+}
